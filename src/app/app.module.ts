57dfb7d9f8fd4fb4f833c8155fcaf0010a9e5b85
katy deng <katydeng@katys-MacBook-Air.local>
2019-05-14 02:33:24 -0700

    importing FireBase Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//firebase modules
+import { AngularFireModule } from '@angular/fire'; 
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule }from '@angular/fire/auth';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase), 
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
